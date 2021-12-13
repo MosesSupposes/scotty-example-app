1c9fff9c534ba4ec143dd8a3af791c7fa653d601
MosesSupposes <mosesasamuel633@gmail.com>
2021-12-12 22:14:31 -0600

    handle more complex requests

--- app/Main.hs
+++ app/Main.hs
@@ -16,3 +16,22 @@ main = scotty 3000 $ do
         text "This was a POST request!"
     put "/" $ do
         text "This was a PUT request!"
+    -- set a header:
+    post "/set-headers" $ do 
+        status status302
+        setHeader "Location" "http://www.google.com.au"
+    -- named parameters:
+    get "/askfor/:word" $ do
+        w <- param "word"
+        html $ mconcat ["<h1>You asked for ", w, ", you got it!</h1>"]
+    -- unnamed parameters from a query string or a form:
+    post "/submit" $ do -- e.g. http://server.com/submit?name=somename
+        name <- param "name"
+        text name
+    -- match a route regardless of the method
+    matchAny "/all" $ do
+        text "matches all methods"
+    -- handler for when there is no matched route
+    -- (this should be the last handler because it matches all routes)
+    notFound $ do
+        text "there is no such route."
